efd3a15812f1d4257a7e4cc320aada4c3e6adb8d
Neuvax <a01640826@tec.mx>
2023-06-02 13:12:45 -0600

    WIP Modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import { Nunito } from 'next/font/google'
 import Navbar from './components/navbar/Navbar';
+import ClientOnly from './components/ClientOnly';
+import Modal from './components/modals/Modal';
 
 
 export const metadata = {
@@ -19,7 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Modal actionLabel= "Submit" title="Hello World" isOpen />
+          <Navbar />``
+        </ClientOnly>
         {children}
       </body>
     </html>
