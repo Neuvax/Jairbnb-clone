802e7b84a89eb2655e17639eb2bb5de0ee6ba877
Neuvax <a01640826@tec.mx>
2023-11-20 18:32:21 -0600

    Map function updated

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -25,7 +25,7 @@ const Map: React.FC<MapProps> = ({ center }) => {
     <MapContainer
       center={(center as L.LatLngExpression) || [51, -0.09]}
       zoom={center ? 4 : 2}
-      scrollWheelZoom={false}
+      scrollWheelZoom={true}
       className="h-[35vh] rounded-lg"
     >
         <TileLayer
